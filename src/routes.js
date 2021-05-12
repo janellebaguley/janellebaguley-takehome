a32245a41e1f42afc84d496b2f53879bf1b45522
janellebaguley <nellie.baguley@gmail.com>
2021-05-12 08:19:13 -0600

    creatingcomponents

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import Furniture from './components/Furniture/Furniture';
+import Designs from './components/Designs/Designs';
+import Cart from './components/Checkout/Cart';
+import Contact from './components/Contact/Contact'
+import Register from './components/Auth/newAuth/Register'
+
+
+export default (
+    <Switch>
+        <Route exact path= '/' component={Home}/>
+        <Route path= '/furniture' component={Furniture}/>
+        <Route path= '/designs' component={Designs}/>
+        <Route path= '/login' component={Auth}/>
+        <Route path= '/cart' component={Cart}/>
+        <Route path= '/contact' component={Contact}/>
+        <Route path = '/register' component={Register}/>
+    </Switch>
+)
